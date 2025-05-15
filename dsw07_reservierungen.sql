b40e649f15f373ec976412ed9e11b0073f9ebcd6
HBKathy <katharina.wallisch@heiterblick.de>
2025-05-15 15:18:24 +0200

    Update dsw07_reservierungen.sql

--- dsw07_reservierungen.sql
+++ dsw07_reservierungen.sql
@@ -1,4 +1,4 @@
-MERGE INTO temp_table_alle_kt_dsw t 
+MERGE INTO temp_table_alle_kt_dsw_mod6 t 
 USING ( 
     SELECT  
         p.teilenr, 
@@ -8,16 +8,7 @@ USING (
     WHERE p.teilenr IN (  
         SELECT DISTINCT a.teilenr  
         FROM allocs a 
-        WHERE a.prodauftr IN (  
-            SELECT prodauftr  
-            FROM ( 
-                SELECT prodauftr  
-                FROM wrkord  
-                CONNECT BY PRIOR prodauftr = linkauf  
-                START WITH prodauftr = 'KA2018120689'  
-  
-) 
-        )  
+        WHERE a.prodauftr LIKE 'KA2018120603%'
         ) 
     AND ar.reserviert_menge > 0 
     GROUP BY p.teilenr 
